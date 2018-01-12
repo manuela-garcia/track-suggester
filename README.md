fa356cd296ea002bfb4bcada9d6184a78a5c8995
Manuela Garcia <manuela.garcia.dev@gmail.com>
2018-01-12 15:29:40 -0800

    add README.md file.

---
+++ README.md
@@ -0,0 +1,31 @@
+# _Track Suggester_
+
+#### _Survey website for Epicodus students, 01/12/18_
+
+#### By _Manuela Garcia_
+
+## Description
+
+_This website help students decide which programming language they would want to specialized while studying at Epicodus._
+
+## Setup/Installation Requirements
+
+* _Clone this repository_
+* _From the project directory, open the index.html file_
+
+
+## Known Bugs
+
+_This website is not completed, so it will not provide anyone with any useful information at the moment._
+
+## Support and contact details
+
+_If you have any questions, please contact me at manuela.garcia.dev@gmail.com_
+
+## Technologies Used
+
+_Html, Css, JavaScript. Bootstrap and jQuery frameworks._
+
+### License
+
+Copyright (c) 2018 **_Manuela Garcia_**
--- js/scripts.js
+++ js/scripts.js
@@ -4,11 +4,21 @@ $(document).ready(function() {
   if (introCompleted) {
     $('#yes-intro').show();
   } else {
-    alert("SORRY!! You must complete the Intro to Programming first before you can move on to any of the programming languages offered by Epidocus");
+    alert("SORRY!! You must complete the Intro to Programming first before you can move on to study any of the programming languages offered by Epidocus");
   }
 
-// the following would be the way to read the value selected
+// the following would be the way to read the value selected. The following commands are not doing anything at the moment.
 
 var design = $("input:radio[name=design]:checked").val();
+
+var largeCompany = $("input:radio[name=design]:checked").val();
+
+var android = $("input:radio[name=design]:checked").val();
+
+var government = $("input:radio[name=design]:checked").val();
+
+var startup = $("input:radio[name=design]:checked").val();
+
+// Again: all these variables are not being read at the moment. I would like to continue working on this when we learn about arrays.
 
 });
