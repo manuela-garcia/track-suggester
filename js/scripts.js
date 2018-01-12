1491bfdbce4e8bf00f4b36e04f41047b398a8541
Manuela Garcia <manuela.garcia.dev@gmail.com>
2018-01-12 15:06:20 -0800

    add radio buttons (user's responses are ignored), add a branch in the very first question

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,14 @@
 $(document).ready(function() {
+  var introCompleted = confirm("Have you completed Intro to Programming? Click OK for YES or Cancel for NO.");
 
+  if (introCompleted) {
+    $('#yes-intro').show();
+  } else {
+    alert("SORRY!! You must complete the Intro to Programming first before you can move on to any of the programming languages offered by Epidocus");
+  }
 
+// the following would be the way to read the value selected
 
-
-
-
-
+var design = $("input:radio[name=design]:checked").val();
 
 });
